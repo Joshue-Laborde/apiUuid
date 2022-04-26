8c9de9f0b0691d2b924716683594a2399d035b10
Joshue-Laborde <joshuelab@gmail.com>
2022-04-26 00:01:17 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// Módulos requeridos
+const express = require('express');
+const http = require('http');
+const helmet = require('helmet');
+var compression = require('compression');
+require('dotenv').config();
+const {v4: uuidv4} = require('uuid');
+
+const app = express();
+app.use(helmet({contentSecurityPolicy: false}));  // Ayuda a proteger aplicaciones Express
+app.use(compression()); //ayuda a la respuesta del cliente sean más rapidas
+
+// Servidor HTTP
+const serverHttp = http.createServer(app);
+serverHttp.listen(process.env.HTTP_PORT, process.env.IP);
+
+// Contenido estático
+app.use(express.static('./public'));
+
+// API
+app.get('/api/get-uuid', function(req, res) {
+    res.send(uuidv4())
+});
+
+// 404
+app.get('*', function(req, res) {
+    res.status(404).send('Error 404 - Recurso no encontrado')
+});
